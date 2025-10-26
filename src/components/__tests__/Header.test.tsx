b88a8b12e90ca7de399a7e334c9f1866ad5cfbfa
Amanda Waite <amandarwaite@gmail.com>
2025-10-26 12:42:34 +0000

    feat: setup jest and react testing library

---
+++ src/components/__tests__/Header.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+
+describe('Header', () => {
+  it('renders the header with navigation links', () => {
+    render(<Header />);
+
+    const heading = screen.getByRole('link', { name: /aisrs/i });
+    expect(heading).toBeInTheDocument();
+
+    const manageLink = screen.getByRole('link', { name: /manage/i });
+    expect(manageLink).toBeInTheDocument();
+
+    const reviewLink = screen.getByRole('link', { name: /review/i });
+    expect(reviewLink).toBeInTheDocument();
+  });
+});
