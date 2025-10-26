d4cb5d655303d4b71f8282cb22ec930d6c6e5095
Amanda Waite <amandarwaite@gmail.com>
2025-10-26 14:36:46 +0000

    docs: update project status in GEMINI.md

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
+  testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testPathIgnorePatterns: ['<rootDir>/tests/'],
 };
